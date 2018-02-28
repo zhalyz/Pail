f3f24d519c250e8cbe7c2e458660f742968ac27f
zhalyz <nsgaming@yandex.ru>
2018-02-28 04:15:15 +0300

    0.0.1b

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import imaplib
 import base64
 import email
 
+
 def TestImapAuthentication(user, auth_string):
     """Authenticates to IMAP with the given auth_string.
 
@@ -14,35 +15,33 @@ def TestImapAuthentication(user, auth_string):
     """
     print('-' * 20)
     imap_conn = imaplib.IMAP4_SSL('imap.yandex.com')
-    imap_conn.debug = 4
+    imap_conn.debug = 0
     imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
-    print('\n{0}'.format(imap_conn.select('INBOX')))
-
-    result, data = imap_conn.uid('search', None, "ALL") # Выполняет поиск и возвращает UID писем.
+    imap_conn.select('INBOX')
+    result, data = imap_conn.uid('search', None, "ALL")  # Выполняет поиск и возвращает UID писем.
     latest_email_uid = data[0].split()[-1]
-    result, data = imap_conn.uid('fetch', latest_email_uid, '(RFC822)')
+    result, data = imap_conn.uid('fetch', latest_email_uid, 'RFC822')  # Вовзращает тело письма
     raw_email = data[0][1]
+    email_message = email.message_from_bytes(raw_email)
+    return get_first_text_block(None, email_message)
 
-    email_message = email.message_from_string(raw_email)
-
-    print(email_message['To'])
-
-    print(email.utils.parseaddr(email_message['From']))# получаем имя отправителя "Yuji Tomita"
-
-    print(email_message.items())# Выводит все заголовки.
 
 def get_first_text_block(self, email_message_instance):
-    maintype = email_message_instance.get_content_maintype()
-    if maintype == 'multipart':
-        for part in email_message_instance.get_payload():
-            if part.get_content_maintype() == 'text':
-                return part.get_payload()
-    elif maintype == 'text':
-        return email_message_instance.get_payload()
+    '''Парсинг последнего сообщения. Возвращает дату,
+    почту получателя, почту отправителя, текст письма
+    '''
+    Date = email_message_instance['Date'][:-6]  # Дата получения письма
+    Receiver = email_message_instance['To']  # Почта получателя
+    Sender = email_message_instance['Envelope-From']  # Почта отправителя
+    Data = email_message_instance.get_payload()  # Тело письма
+    return Date, Receiver, Sender, Data
 
-def GenerateOAuth2String(username, access_token, base64_encode=True):
-    """Generates an IMAP OAuth2 authentication string.
 
+def GenerateOAuth2String(username, access_token):
+    """Генерирует Oauth2 строку
+    Base64 не нужна, так как imap4 сам шифрует
+    Generates an IMAP OAuth2 authentication string.
+    
     See https://developers.google.com/google-apps/gmail/oauth2_overview
 
     Args:
@@ -55,12 +54,10 @@ def GenerateOAuth2String(username, access_token, base64_encode=True):
     """
     auth_string = 'user={0}\@yandex.ru\001auth=Bearer {1}\001\001'.format(username, access_token)
     auth_string = str.encode(auth_string)
-    if base64_encode:
-        auth_string = base64.b64encode(auth_string)
     return auth_string
+
 
 if __name__ == '__main__':
     user = 'alexander.shiplen'
     access_token = 'AQAAAAAj6ki9AATVmv5dS7d1c0_YkkIr_UV2PKw'
-
-    TestImapAuthentication(user, GenerateOAuth2String(user, access_token, base64_encode = False))
+    Date, Receiver, Sender, Data = TestImapAuthentication(user, GenerateOAuth2String(user, access_token))
